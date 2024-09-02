010ba4b4b3328b3523f7ff5756caec6a047d11d2
Benn Mapes <benn@elevatedsignals.com>
2024-09-02 15:05:21 -0700

    Updates for FAQ

--- src/lib/faq.tsx
+++ src/lib/faq.tsx
@@ -20,8 +20,16 @@ const questions = [
       "Warm layers (wool)",
       "Waterproof layers",
       "Swimsuit",
+      "Bugspray",
     ],
     icon: "check",
+  },
+  {
+    question: "Are children invited?",
+    answers: [
+      "Yes! We're so excited to have your little ones join us. Please let us know if you're bringing any children so we can plan accordingly.",
+    ],
+    icon: "info",
   },
   {
     question: "What kind of attire is this affair?",
@@ -31,9 +39,23 @@ const questions = [
     icon: "info",
   },
   {
+    question: "Will the wedding be indoors or outdoors?",
+    answers: [
+      "The wedding will be primarily outdoors, weather permitting. The ceremony will take place on the lawn at Tamarack Farm. The reception dinner will be indoors in the farmhouse, with dancing in an outdoor covered area, the Dance Barn, located next to the farmhouse.",
+    ],
+    icon: "info",
+  },
+  {
     question: "What shouldn’t I bring to camp?",
     answers: ["Pets", "Drugs", "Alcohol (don’t worry, we’ve got you covered!)"],
     icon: "ban",
+  },
+  {
+    question: "Will there be food for people with dietary restrictions?",
+    answers: [
+      "Yes, we plan to offer a variety of foods to accommodate everyone attending, including vegetarian, gluten-free, and dairy-free options. If you have specific allergens or dietary needs, please contact Benn or Elena, and we’ll do our best to ensure you have something to eat!",
+    ],
+    icon: "info",
   },
   {
     question: "Where should we park for the weekend?",
@@ -50,15 +72,22 @@ const questions = [
   {
     question: "Where is your registry?",
     answers: [
-      "Just kidding! We have enough stuff, just bring yourselves and your dancing shoes!",
+      "Just kidding! Your presence is the best gift, just bring yourselves and your dancing shoes!",
     ],
     gif: confusedGif,
   },
   {
     question: "A few camp rules to note…",
     answers: [
-      "All waterfront activities on F&W property require a certified lifeguard. We’ll set aside a block of time when a lifeguard is around so we can swim or explore the lake via canoe.",
+      "All waterfront activities on F&W property require a certified lifeguard. We’ll set aside a block of time when a lifeguard is around so we can swim or explore the lake via canoe. If you enter the lake from an F&W access point without a lifeguard on duty, you will be fined.",
       "F&W requires that we keep any drinking to a few designated areas: the main lodge/ dining hall & surrounding porch and immediate lawn area, as well as the dance barn and recreation lodge. No alcohol is permitted on the main road, in parking areas, cabins/sleeping spaces or areas below the main road (lake side) including the waterfront areas.",
+    ],
+    icon: "info",
+  },
+  {
+    question: "What about the weather?",
+    answers: [
+      "No guarantees on what the weather will bring yet, but we do tend to see some rain this time of year. Temperatures can be anywhere between 70°F during the day to 40°F at night. Keep an eye on the weather for Plymouth, VT (or Killington) in the weeks leading up to the wedding for a better understanding.",
     ],
     icon: "info",
   },
@@ -77,14 +106,19 @@ function Icon({ icon = "check", size = 10, ...props }) {
 export function FAQ({ ...props }) {
   return (
     <div {...props}>
-      <Accordion type="multiple" className="max-w-sm mx-auto pt-5">
+      <Accordion type="multiple" className="max-w-md mx-auto pt-5">
         {questions.map((question, index) => (
           <AccordionItem key={index} value={String(index)}>
-            <AccordionTrigger>{question.question}</AccordionTrigger>
+            <AccordionTrigger className="text-xl">
+              {question.question}
+            </AccordionTrigger>
             <AccordionContent>
               <ul>
                 {question.answers.map((answer, index) => (
-                  <li key={index} className="items-center text-left pb-3">
+                  <li
+                    key={index}
+                    className="items-center text-left pb-3 text-lg"
+                  >
                     {question.gif && <img src={question.gif}></img>}
 
                     {question.icon && (
