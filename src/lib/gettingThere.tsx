55f4be93b1b095123a8ea5239bfb1156f2e6fd89
Benn Mapes <benn@elevatedsignals.com>
2024-09-17 09:45:20 -0700

    Added more travel details

--- src/lib/gettingThere.tsx
+++ src/lib/gettingThere.tsx
@@ -11,10 +11,10 @@ export function GettingThere({ ...props }) {
   return (
     <div {...props}>
       <section>
-        <div className="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
+        <div className="py-4 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
           <div className="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-2 md:gap-12 md:space-y-0">
             <div>
-              <div className="flex justify-center items-center mb-4 w-10 h-10 lg:h-12 lg:w-12 mx-auto">
+              <div className="flex justify-center items-center mb-2 w-10 h-10 lg:h-12 lg:w-12 mx-auto">
                 <Plane size={35} className="flex-shrink-0" />
               </div>
               <h3 className="mb-2 text-xl font-bold dark:text-white">
@@ -30,15 +30,21 @@ export function GettingThere({ ...props }) {
               </p>
             </div>
             <div>
-              <div className="flex justify-center items-center mb-4 w-10 h-10 lg:h-12 lg:w-12 mx-auto">
+              <div className="flex justify-center items-center mb-2 w-10 h-10 lg:h-12 lg:w-12 mx-auto">
                 <CarFront size={35} className="flex-shrink-0" />
               </div>
               <h3 className="mb-2 text-xl font-bold dark:text-white">By car</h3>
               <p className="text-lg text-gray-500 dark:text-gray-400">
-                Follow I-93 N and I-89 N to US-4 W/Woodstock Rd in Hartford.
+                From Boston, follow I-93 N and I-89 N. From northern Vermont, follow I-89 S.
                 <br />
-                Take exit 1 from I-89 N. Continue on US-4 W. <br />
-                Drive to Farm And Wilderness Rd in Plymouth.
+                Get off at Exit 1 in Vermont, US-4 Woodstock/Rutland.
+                <br />
+                Take US-4 West for ~23 mi to US-100 towards Plymouth/Ludlow (on the left)
+                <br />
+                Drive 2 mi to Farm And Wilderness Rd (on the left) in Plymouth.
+                <br />
+                The Farm is 0.8 mi down Farm And Wilderness Rd on the left.
+                <address>869 Farm And Wilderness Road, Plymouth, VT</address>
               </p>
             </div>
           </div>
